c1e99ee04b024972cf4893f40b759e3e71e211e2
Lukasz Wisniewski <lukaszwisniewski1236@gmail.com>
2023-02-18 18:50:39 +0100

    1.1

--- Source.cpp
+++ Source.cpp
@@ -4,7 +4,6 @@
 #include <string>
 
 std::ofstream oferty("oferty_pracy.csv"); //Plik csv do ktorego trafia linki do ofert pracy
-int licznik = 0; //Z powodu ze kazda oferta ma 2 linki zamiast 1 musze uzyc licznika aby unikac duplikatow
 
 std::string extract_html() { //Zdobywa zawartosc storny w string za pomoca cpr
 	cpr::Url adres = cpr::Url{ "https://www.pracuj.pl/praca/bialystok;wp?rd=30&cc=5015%2C5016&et=17&tc=0" };
@@ -12,7 +11,7 @@ std::string extract_html() { //Zdobywa zawartosc storny w string za pomoca cpr
 	return odpowiedz.text;
 }
 
-void szukaj_link_praca(GumboNode* node) { //Funkcja do wypisania linkow do pliky tekstowego
+void szukaj_link_praca(GumboNode* node,int& licznik) { //Funkcja do wypisania linkow do pliky tekstowego
 	if (node->type != GUMBO_NODE_ELEMENT) return;
 
 	if (node->v.element.tag == GUMBO_TAG_A) {
@@ -32,17 +31,19 @@ void szukaj_link_praca(GumboNode* node) { //Funkcja do wypisania linkow do pliky
 
 	GumboVector* children = &node->v.element.children;
 	for (unsigned int i = 0; i < children->length; i++) {
-		szukaj_link_praca(static_cast<GumboNode*>(children->data[i]));
+		szukaj_link_praca(static_cast<GumboNode*>(children->data[i]),licznik);
 	}
 }
 
 
 int main() {
+	int licznik = 0; //Z powodu ze kazda oferta ma 2 linki zamiast 1 musze uzyc licznika aby unikac duplikatow
+
 	std::string zawartosc = extract_html();
 
 	GumboOutput* zawartosc_gumbo = gumbo_parse(zawartosc.c_str());
 
-	szukaj_link_praca(zawartosc_gumbo->root);
+	szukaj_link_praca(zawartosc_gumbo->root,licznik);
 	
 	oferty.close();
 
